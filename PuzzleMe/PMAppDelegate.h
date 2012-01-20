3aa476acf29f9d1cd8350688668b0daa83b094fa
INSA-TC <namla@eduroinsa464.insa-lyon.fr>
2012-01-20 17:10:51 +0100

    Initial Commit

---
+++ PuzzleMe/PMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PMAppDelegate.h
+//  PuzzleMe
+//
+//  Created by Nam LA on 1/20/12.
+//  Copyright (c) 2012 INSA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PuzzleMe/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PuzzleMe
+//
+//  Created by Nam LA on 1/20/12.
+//  Copyright (c) 2012 INSA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PMAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PMAppDelegate class]));
+    }
+}
